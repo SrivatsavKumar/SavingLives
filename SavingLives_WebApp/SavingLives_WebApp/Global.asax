7051764b12e2503cb7dd40ab853620625c7bede8
Priya Adabala <priyaadabala123@gmail.com>
2020-02-11 16:48:14 +0530

    Adding SavingLives to LocalRepository

---
+++ SavingLives_WebApp/SavingLives_WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SavingLives_WebApp.MvcApplication" Language="C#" %>
---
+++ SavingLives_WebApp/SavingLives_WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SavingLives_WebApp.Startup))]
+namespace SavingLives_WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
